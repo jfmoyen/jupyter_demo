c6f1b3d3b4c6c9a3a9e6929dada96dedf2b50c55
jfmoyen <jfmoyen@gmail.com>
2022-10-18 10:55:14 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,7 @@
+# Tidyverse packages, for demo
 install.packages("tidyverse")
 install.packages("readxl")
+
+# Install GCDkit (and dependencies)
+install.packages(c("curl","R2HTML","rgdal","RODBC","sp","tkrplot","XML"))
+install.packages("./install/GCDkit_6.1.zip")
