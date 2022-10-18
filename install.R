1f9f58b97d171affaf556747fbaf9e64ee66b7a7
jfmoyen <jfmoyen@gmail.com>
2022-10-18 22:33:22 +0200

    Update install.R

--- install.R
+++ install.R
@@ -7,6 +7,7 @@ install.packages(c("curl","R2HTML","rgdal","RODBC","sp","XML"))
 
 # GCDkit should be on gcdkit.org, as an interim solution let's try a dropbox link...
 # install.packages("http://www.gcdkit.org/sw/GCDkit_6.1.zip",repos=NULL)
-install.packages("https://dl.dropboxusercontent.com/s/zvrk4nra486qa8n/GCDkit_6.1.tar.gz?dl=0",repos=NULL)
+install.packages("https://dl.dropboxusercontent.com/s/zvrk4nra486qa8n/GCDkit_6.1.tar.gz?dl=0",repos=NULL, INSTALL_opts = "--no-test-load")
+# No test load does not seem a very nice solution, but...
 
 
