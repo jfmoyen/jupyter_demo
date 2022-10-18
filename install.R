83fdb4d8e8b28e21253eca859dd3395144bc43e5
jfmoyen <jfmoyen@gmail.com>
2022-10-18 14:50:04 +0200

    Update install.R

--- install.R
+++ install.R
@@ -7,6 +7,6 @@ install.packages(c("curl","R2HTML","rgdal","RODBC","sp","tkrplot","XML"))
 
 # GCDkit should be on gcdkit.org, as an interim solution let's try a dropbox link...
 # install.packages("http://www.gcdkit.org/sw/GCDkit_6.1.zip",repos=NULL)
-install.packages("https://www.dropbox.com/s/zvrk4nra486qa8n/GCDkit_6.1.tar.gz?dl=0",repos=NULL)
+install.packages("https://dl.dropboxusercontent.com/s/zvrk4nra486qa8n/GCDkit_6.1.tar.gz?dl=0",repos=NULL)
 
 
